e7e49c577b935677fb24c78688cbbd54105eb4d4
Shadehawke <35213273+Shadehawke@users.noreply.github.com>
2023-03-01 09:03:56 -0600

    Added JavaScript Projects

---
+++ JavaScript Projects/lunchbuyer.js
@@ -0,0 +1,7 @@
+function whosPaying(names) {
+
+        var randomPerson = names[Math.floor(Math.random()*names.length)]
+        return randomPerson + " is going to buy lunch today!"
+        
+   
+}
